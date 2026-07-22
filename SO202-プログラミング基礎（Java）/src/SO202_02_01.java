34ee0089ef8e181da5326ad6ea65ab7fd7509266
fukaryu2006-bit <fukaryu2006@gmail.com>
2026-07-22 20:06:50 +0900

    Fix typo in print statement in SO202_02_01.java

--- SO202-プログラミング基礎（Java）/src/SO202_02_01.java
+++ SO202-プログラミング基礎（Java）/src/SO202_02_01.java
@@ -32,7 +32,7 @@
 public class SO202_01_01{
     public static void main (String[]args){
         for (int i=0;i<10;i++ ){
-            System,out.println("これはJavaの例文です。");
+            System.out.println("これはJavaの例文です。");
         }
     }
 }
