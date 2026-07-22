c770ff7a9af77b0b238bcf467f680f7492cbbdc1
fukaryu2006-bit <fukaryu2006@gmail.com>
2026-07-22 20:11:10 +0900

    Rename class from SO202_01_01 to SO202_02_01

--- SO202-プログラミング基礎（Java）/src/SO202_02_01.java
+++ SO202-プログラミング基礎（Java）/src/SO202_02_01.java
@@ -29,7 +29,7 @@
 
 // ここより下に解答を記載する。
 
-public class SO202_01_01{
+public class SO202_02_01{
     public static void main (String[]args){
         for (int i=0;i<10;i++){
             System.out.println("これはJavaの例文です。");
