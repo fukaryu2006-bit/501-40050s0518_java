88d16dd3946990a8e01d716ffd062ebe5c351008
fukaryu2006-bit <fukaryu2006@gmail.com>
2026-07-22 20:04:37 +0900

    Update SO202_02_01.java

--- SO202-プログラミング基礎（Java）/src/SO202_02_01.java
+++ SO202-プログラミング基礎（Java）/src/SO202_02_01.java
@@ -28,3 +28,11 @@
 
 
 // ここより下に解答を記載する。
+
+public class SO202_01_01{
+    public static void main (String[]args){
+        for (int i=0;i<10 i++ ){
+            System,out.println("これはJavaの例文です。");
+        }
+    }
+}
