2f78b0054d24e880e8b48d236e825b531f8c344f
fukaryu2006-bit <fukaryu2006@gmail.com>
2026-07-22 20:05:22 +0900

    Fix syntax error in for loop in SO202_02_01.java

--- SO202-プログラミング基礎（Java）/src/SO202_02_01.java
+++ SO202-プログラミング基礎（Java）/src/SO202_02_01.java
@@ -31,7 +31,7 @@
 
 public class SO202_01_01{
     public static void main (String[]args){
-        for (int i=0;i<10 i++ ){
+        for (int i=0;i<10;i++ ){
             System,out.println("これはJavaの例文です。");
         }
     }
