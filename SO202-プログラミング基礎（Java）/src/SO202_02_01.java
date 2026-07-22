fda89f1fb97389a55747aa94ae2434e8bed9d15c
fukaryu2006-bit <fukaryu2006@gmail.com>
2026-07-22 20:08:10 +0900

    Fix class declaration and loop syntax in SO202_02_01

--- SO202-プログラミング基礎（Java）/src/SO202_02_01.java
+++ SO202-プログラミング基礎（Java）/src/SO202_02_01.java
@@ -29,9 +29,9 @@
 
 // ここより下に解答を記載する。
 
-public class SO202_01_01{
+public class SO202_01_01.java{
     public static void main (String[]args){
-        for (int i=0;i<10;i++ ){
+        for (int i=0;i<10;i++){
             System.out.println("これはJavaの例文です。");
         }
     }
